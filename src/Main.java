a61293f4e73d4a0c5dfd8e1d781934e2ca478c9f
ViktorMakTorwend <vikmactor@gmail.com>
2025-01-14 11:39:30 +0300

    "Init"

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+package  src;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world");
+//        int a = 10000000;
+//        float b = 3.14f;
+//        double c = 10.64654654;
+//        boolean d = false;
+//        System.out.println(a);
+
+//        String name = "Andrei";
+//        String name1 = "Andrei";
+//        String name2 = new String("Andrei");
+//        System.out.println(name == name1);  // true
+//        System.out.println(name == name2);  // false
+
+//        int a = 1;
+//        int b = 2;
+//        double c = (double) a / b;
+//        System.out.println(c);  // 0.5
+
+
+    }
+}
